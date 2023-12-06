ea13fe5321d94cf7c4bfe93a6b2c4b862d25a208
Christian Kramer <ck.daill@gmail.com>
2023-12-06 11:15:57 +0100

    transfer

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,21 +1,59 @@
 extern crate core;
 
+use regex::Regex;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, Read};
 
-fn read_from_file() -> Vec<String> {
+fn read_from_file() -> (Vec<i32>, HashMap<String, Vec<(i32, i32, i32)>>) {
     let mut file = File::open("input");
-    let result: Vec<String> = match file {
+    let result: (Vec<i32>, HashMap<String, Vec<(i32, i32, i32)>>) = match file {
         Ok(file) => {
             let lines = io::BufReader::new(file).lines();
-            let mut result: Vec<String> = Vec::new();
+            let mut result: HashMap<String, Vec<(i32, i32, i32)>> = HashMap::new();
+            let mut seeds: Vec<i32> = Vec::new();
+
+            let re = Regex::new(r"([0-9]+)\s([0-9]+)\s([0-9]+)").unwrap();
+
             for line in lines {
                 if let Ok(line) = line {
-                    result.push(return_line);
+                    if line.starts_with("seeds:") {
+                        seeds = line
+                            .split(" ")
+                            .filter_map(|s| s.parse::<i32>().ok())
+                            .collect();
+                    } else {
+                        let mut current_line = "".to_string();
+
+                        if line.ends_with("map:") {
+                            current_line = line.clone();
+                            continue;
+                        }
+
+                        let mut numbers = Vec::new();
+
+
+                        if !line.is_empty() {
+                            let Some(caps) = re.captures(&line) else {
+                                continue;                                
+                            };
+                            let (a, b, c) = (
+                                &caps[1].parse::<i32>().unwrap(),
+                                &caps[2].parse::<i32>().unwrap(),
+                                &caps[3].parse::<i32>().unwrap(),
+                            );
+                            numbers.push((*a, *b, *c));
+                            println!("{:?}", numbers);
+                        } else {
+                            result.insert(current_line.to_string(), numbers);
+                            println!("{:?}", result);
+                            current_line = "".to_string();
+                        }
+                    }
                 }
             }
-            result
+            (seeds, result)
         }
         Err(e) => panic!("Cannot process file: {}", e),
     };
@@ -23,6 +61,6 @@ fn read_from_file() -> Vec<String> {
 }
 
 fn main() {
-    let content: Vec<String> = read_from_file();
-    println!("{}", sum);
+    let content: (Vec<i32>, HashMap<String, Vec<(i32, i32, i32)>>) = read_from_file();
+    println!("{:?}", content);
 }
